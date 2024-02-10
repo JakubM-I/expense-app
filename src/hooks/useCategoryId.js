5f6d5979a705271d27fbd7b5124647152d32cbde
Jakub Musialowski <mds@mds.biz.pl>
2024-02-10 22:48:29 +0100

    Adding protection against passing on an empty variable

--- src/hooks/useCategoryId.js
+++ src/hooks/useCategoryId.js
@@ -6,8 +6,10 @@ export const useCategoryId = (category) => {
     const [catList] = useCategory();
     const [categoryId, setCategoryId] = useState("");
 
+    const selectedCategory = category ? category.toLowerCase() : "";
+
     useEffect(() => {
-        const selectCategory = catList.filter(cat => cat.categoryName.toLowerCase() === category.toLowerCase())
+        const selectCategory = catList.filter(cat => cat.categoryName.toLowerCase() === selectedCategory)
         
         setCategoryId(selectCategory.length > 0 ? selectCategory[0].id : "")
     }, [category, catList]);
