81593125ce5a58776dc9cadad865ced4cf2e79fd
Jakub Musialowski <mds@mds.biz.pl>
2024-02-18 23:43:43 +0100

    Modal animation chnaged

--- src/common/Modal/styled.js
+++ src/common/Modal/styled.js
@@ -1,4 +1,4 @@
-import styled, {css} from 'styled-components';
+import styled, {css, keyframes} from 'styled-components';
 
 export const StyledBackdrop = styled.div`
     position: fixed;
@@ -11,8 +11,14 @@ export const StyledBackdrop = styled.div`
     transition: background 0.3s linear;
 
     ${({isOpen}) => isOpen && css`
-        visibility: visible;
-        background: #0000005c;
+        /* visibility: visible; */
+        /* background: #0000005c; */
+        animation: ${openModal} 0.3s 0.1s linear forwards;
     ` }
 `;
+
+const openModal = keyframes`
+    0%{visibility: hidden;}
+    100%{visibility: visible; background: #0000005c;}
+`
 
