d441d63ebcabef89c3dcbfb000960b35a053fbe8
Jakub Musialowski <mds@mds.biz.pl>
2024-08-23 23:32:11 +0200

    Change router to current version

--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ import Root from "./features/app/Root";
 import Expenses from "./features/expense-list/Expenses";
 import Categories from "./features/categories/Categories";
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path="/expense-app" element={<Root />}>
-    <Route index element={<Expenses />}/>
-    <Route path="categories" element={<Categories />}/>
-  </Route>
-))
+// const router = createBrowserRouter(createRoutesFromElements(
+//   <Route path="/expense-app" element={<Root />}>
+//     <Route index element={<Expenses />}/>
+//     <Route path="categories" element={<Categories />}/>
+//   </Route>
+// ))
+
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    path: "/expense-app",
+    children: [
+      {
+        element: <Expenses />,
+        path: "/expense-app",
+      },
+      {
+        element: <Categories />,
+        path: "categories"
+      }
+    ],
+  }
+])
 
 function App() {
   return (
